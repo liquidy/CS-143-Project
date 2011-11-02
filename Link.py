d0087dda662f2df6c3768260650f7f2c91760564
judymou <moujudy@gmail.com>
2011-11-02 15:54:26 -0700

    Add Packet and Link Class

---
+++ Link.py
@@ -0,0 +1,30 @@
+from SimPy.Simulation import *
+
+class Link(Process):
+
+    def __init__(self,linkID, linkRate, start, end, propTime, price):
+        Process.__init__(self, name="Link"+str(linkID))
+        self.queue = []
+        self.linkID = linkID
+        self.linkRate = linkRate
+        self.start = start
+        self.end = end
+        self.propTime = propTime
+        self.price = price
+        self.active = False
+
+    # if link is active, self.active = True, otherwise, self.active = False
+    # Source can use this attribute to decide if it needs to reactivate the  
+    # link
+    def run(self):
+        while True:
+            if self.queue == []:
+                self.active = False
+                yield passivate, self
+            self.active = True
+            packet=self.queue.pop(0)
+            # wait for trans time
+            yield hold, self, packet.size/self.linkRate
+            # packet start propagate in the link
+            packet.run(self.propTime, self.end)
+            print "t = %.2f: Job-%d departs" % (now(), packet.packetID)
---
+++ Packet.py
@@ -0,0 +1,47 @@
+from SimPy.Simulation import *
+
+#packet:
+#- packet is a process
+#- source creates packet object (at first packet is not activated), enqueues it in the #link
+#- link then tells it where it is going (in terms of next device) and tells it how long #to wait, and then activates the packet
+#- packet waits for the needed time
+#- once it has waited for that time it enqueues itself in that appropriate device #(router) and goes to sleep
+#- packet needs public router object
+#- when packet wakes up, it is because a link has reactivated it
+
+#Packet:
+#- int ID, int size, int timeSent
+#- int sourceID, int destinationID
+#- Boolean isRouterMessage, Boolean acknowledgement
+#- Object Message (RouterMessage, Other)
+
+class Packet(Process):
+    # To create a Packet(process), use p1 = Packet(1, 20, 1)
+    # to create a packet with ID = 1, size = 20, timeSent = 1
+    def __init__(self, packetID, size, timeSent, sourceID, desID, isRouterMesg, isAck, myMessage):
+        Process.__init__(self, name="Packet"+str(packetID))
+        self.packetID= packetID
+        self.size = size
+        self.timeSent = timeSent
+        self.sourceID = sourceID
+        self.desID = desID
+        self.isRouterMesg = isRouterMesg
+        self.isAck = isAck
+        self.myMessage = myMessage
+        
+    # to activate the process, call activate(p1, p1.run())
+    # I understand why link should be active-passivate-active...
+    # but I think packet could always stay active, I don't think I see
+    # why we need to make it passivate and wake up again.
+    def run(self, propTime, device):
+        # reactivate(self)
+        yield hold, self, propTime
+        device.bufferEnqueue(self)
+        # yield passivate, self
+        
+    
+        
+        
+        
+        
+        
