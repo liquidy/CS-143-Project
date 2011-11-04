2a3f1ec9b2dd09fcb1c9254d106420cab08538fa
judymou <moujudy@gmail.com>
2011-11-03 19:18:17 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -107,9 +107,6 @@ class Link(Process):
         self.packetsSent = 0        
         self.active = False
 
-    # if link is active, self.active = True, otherwise, self.active = False
-    # Source can use this attribute to decide if it needs to reactivate the  
-    # link
     def run(self):
         while True:
             if self.queue == []:
@@ -130,24 +127,8 @@ class Link(Process):
             reactivate(packet)
             print "t = %.2f: Job-%d departs" % (now(), packet.packetID)        
 
-#packet:
-#- packet is a process
-#- source creates packet object (at first packet is not activated), enqueues it in the #link
-#- link then tells it where it is going (in terms of next device) and tells it how long #to wait, and then activates the packet
-#- packet waits for the needed time
-#- once it has waited for that time it enqueues itself in that appropriate device #(router) and goes to sleep
-#- packet needs public router object
-#- when packet wakes up, it is because a link has reactivated it
-
-#Packet:
-#- int ID, int size, int timeSent
-#- int sourceID, int destinationID
-#- Boolean isRouterMessage, Boolean acknowledgement
-#- Object Message (RouterMessage, Other)
-
 class Packet(Process):
-    # To create a Packet(process), use p1 = Packet(1, 20, 1)
-    # to create a packet with ID = 1, size = 20, timeSent = 1
+    
     def __init__(self, packetID, timeSent, sourceID, desID, isRouterMesg, isAck, myMessage):
         Process.__init__(self, name="Packet" + str(packetID))
         self.packetID= packetID
@@ -161,10 +142,6 @@ class Packet(Process):
         self.propTime = None
         self.device = None
         
-    # to activate the process, call activate(p1, p1.run())
-    # I understand why link should be active-passivate-active...
-    # but I think packet could always stay active, I don't think I see
-    # why we need to make it passivate and wake up again.
     def run(self):
         while True:
             yield passivate, self
