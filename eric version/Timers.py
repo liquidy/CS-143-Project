8bb3d6c62e4e6c9adfb4d56977979e113643aa7e
msushkov <sushkovm@gmail.com>
2011-12-06 02:35:40 -0800

    Update eric version/Timers.py

--- eric version/Timers.py
+++ eric version/Timers.py
@@ -13,7 +13,7 @@ from Global import *
 class RoutingTimer(Process):
 
     def __init__(self, router):
-        Process.__init__(self, name="RoutingTimer " + str(router.ID))
+        Process.__init__(self, name = "RoutingTimer " + str(router.ID))
         self.router = router
         self.time = router.globs.PROBE_RATE
         
@@ -25,26 +25,30 @@ class RoutingTimer(Process):
             self.router.timerHandler()
 
             
-            
-# SourceTimer
-# this is called every time a packet is sent. it waits the timeout
-# set by the source, then resends packet while it is in list of outst
-# packets. once it is no longer in that list, just passivate
+################################################################################
+#   SourceTimer                                                                #
+################################################################################
+
+# There is a SourceTimer object associated with each packet, so
+# this is instantiated every time a packet is sent. This timer waits the timeout
+# time set by the source, then resends packet while it is in list of outstanding
+# packets. Once it is no longer in that list, the timer just passivates.
 class SourceTimer(Process):
-    # packet is the packet for which this timer is associated with
-    # time is the length of time before timer times out
-    # source is the object which sends the packets
+
     def __init__(self, packet, time, source):
-        Process.__init__(self, name="Timer" + str(packet.packetID))
-        self.packet = packet
-        self.time = time
-        self.source = source
+        Process.__init__(self, name = "Timer" + str(packet.packetID))
+        self.packet = packet     # the packet which this timer is associated with
+        self.time = time         # the length of time before timer times out
+        self.source = source     # the object which sends the packets
     
     def run(self):
+        # While we still have outstanding packets
         while self.packet.packetID in self.source.outstandingPackets:
+            # wait until we reacht the timeout time
             yield hold, self, self.time
+            # at this point we have a timeout
             
-            # while we havent received an ack for the packet
+            # While we havent received an ack for the packet
             if self.packet.packetID in self.source.outstandingPackets:
                 # When time out, update variables to slow start
                 self.source.enabledCCA = False
@@ -54,17 +58,20 @@ class SourceTimer(Process):
                 
                 self.source.missingAck += 1
                     
-                #Add packet to source's queue for retransmit the timeout packet
+                # Add packet to source's queue to retransmit the timeout packet
                 self.source.toRetransmit.append(self.createPacket(self.packet))
+                
+                # Make sure source is activated
                 if not self.source.active:
                     self.source.active = True
                     reactivate(self.source)
         yield passivate, self
             
-    # copy of packet with this id
+    # 
+    # Create a copy of packet with this id
+    #
     def createPacket(self, packet):
         message = "Packet " + str(packet.packetID) + "'s data goes here!"
-        # packetId, timesent, sourceID, destid, isroutermsg, isack, msg
         newPacket = Packet(packet.packetID, now(), packet.sourceID,
                            packet.desID, packet.isRouterMesg, packet.isAck, message, self.source.globs)
         activate(newPacket, newPacket.run())
